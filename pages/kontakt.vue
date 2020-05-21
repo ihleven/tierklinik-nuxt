205020ae45fc3b81f450c25c53409572ef87baa6
ih leven <github@ihleven.de>
2020-05-21 11:19:23 +0200

    Kontektseite überarbeitet, Bulma auf 0.8.2

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -3,30 +3,62 @@
         <!-- <Navigation /> -->
         <PageHero :image="image" :heading="title" :subheading="subtitle"></PageHero>
 
-        <section class="section">
-            <div style="width: 700px;position: relative;">
-                <iframe
-                    width="700"
-                    height="440"
-                    src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=6714%20N%C3%BCziders%2C%20Elserweg%208a+(Tierklinik%20Tschabrun)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
-                    frameborder="0"
-                    scrolling="no"
-                    marginheight="0"
-                    marginwidth="0"
-                ></iframe>
-                <div style="position: absolute;width: 80%;bottom: 10px;left: 0;right: 0;margin-left: auto;margin-right: auto;color: #000;text-align: center;">
-                    <small style="line-height: 1.8;font-size: 2px;background: #fff;"
-                        >Powered by <a href="http://www.googlemapsgenerator.com/pt/">googlemapsgen (pt)</a> &
-                        <a href="https://www.buyinstagramfollowersreviews.net/">5 ways to clean up your twitter pro buy followers guide</a></small
-                    >
+        <section class="section wegbeschreibung">
+            <div class="tile is-ancestor">
+                <div class="tile is-vertical ">
+                    <article class="tile is-child box">
+                        <h4 class="title is-5">
+                            Mit dem
+                            <a
+                                href="https://maps.google.com/maps?ll=47.166453,9.815448&z=16&t=m&hl=de&gl=DE&mapclient=embed&daddr=Tierklinik%20Dr%20Sylvia%20u%20Dr%20Rudolf%20Tschabrun%20Elserweg%208%206714%20N%C3%BCziders%20%C3%96sterreich@47.1664532,9.815448"
+                                target="_blank"
+                                >Routenplaner</a
+                            >
+                            finden Sie am Schnellsten zu uns:
+                        </h4>
+
+                        <p class="subtitle">With some content</p>
+                        <div class="content">
+                            <ul>
+                                <li>Klicken Sie in der Karte auf den blauen Pfeil mit der Beschriftung "Routenp..."</li>
+                                <li>Wählen Sie dort das gewünschte Verkehrsmittel und geben Sie Ihre Adresse ein (Straße Hausnummer Komma PLZ und/oder Ort)</li>
+                            </ul>
+                        </div>
+                    </article>
                 </div>
-                <style>
-                    #gmap_canvas img {
-                        max-width: none !important;
-                        background: none !important;
-                    }
-                </style>
+            
+                <div class="tile is-parent">
+                    <article class="tile is-child box autobahn">
+                        <h4 class="title is-5">Von der Autobahnabfahrt Nüziders:</h4>
+                        <p>
+                            Auf der Dorfhauptstrasse an der Kirche und dem Dorfzentrum vorbei Richtung Bludenz, nach Ortsende Nüziders links hinauf, etwa 1 km bergauf dann rechts abbiegen und vor dem
+                            Torbogen links hinauf auf den Elserweg. Der Weg ist mit weissen TIERKLINIK-Schildern bezeichnet.
+                        </p>
+                    </article>
+                    <div class="tile is-child box autobahn">
+                        <h4 class="title is-5">Von der Autobahnabfahrt Bludenz</h4>
+                        <p>
+                            Autobahnausfahrt Bludenz / Bürs, dann Richtung Bludenz über eine große Brücke über die Bahngeleise. Bei der nächsten Kreuzung nach rechts abbiegen und leicht aufwärts durch
+                            die Stadt Bludenz fahren. Sie fahren an der Fußgängerzone (rechts) geradeaus vorbei. Nach ca 150 Metern ist ein Schild "Nüziders" nach links. Auf dieser Strasse ca einen
+                            Kilometer fahren, bis direkt hinter einer Brücke ein grünes Schild "Tierklinik" nach rechts oben weist. Weiter durch einen Torbogen, dann gleich links wieder durch einen
+                            Torbogen (einspurig!) und nach 30 m rechts nach oben (Schild Tierklinik). Unser Parkplatz ist nach 80 m auf der rechten Seite.
+                        </p>
+                    </div>
+                </div>
             </div>
+        </section>
+
+        <section class="hero">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2712.5609271477006!2d9.813253951545752!3d47.16645317905578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479b4f97ee46ce5b%3A0x38f59ab56f432331!2sTierklinik%20Dr%20Sylvia%20u%20Dr%20Rudolf%20Tschabrun!5e0!3m2!1sde!2sde!4v1589391753052!5m2!1sde!2sde"
+                width="700"
+                height="440"
+                frameborder="0"
+                style="width:100%; height: 450px; border:0;"
+                allowfullscreen=""
+                aria-hidden="false"
+                tabindex="0"
+            ></iframe>
         </section>
 
         <Footer />
@@ -74,4 +106,11 @@
     }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+    .autobahn {
+        p {
+            font-size: 1rem;
+            font-weight: 300;
+        }
+    }
+</style>
